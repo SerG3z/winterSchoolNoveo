395a2add94700f107615f397df3cc517014f09b1
serg <serg3z.hard@gmail.com>
2016-03-06 17:13:40 +0600

    issued date input in a separate class

---
+++ university-android-task1/app/src/main/java/com/noveogroup/tas1/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.noveogroup.tas1;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by serg on 06.03.16.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    TextView txDate;
+
+    public DatePickerFragment(View v) {
+        txDate = (TextView)v;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        txDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+    }
+}
--- university-android-task1/app/src/main/java/com/noveogroup/tas1/MainActivity.java
+++ university-android-task1/app/src/main/java/com/noveogroup/tas1/MainActivity.java
@@ -1,16 +1,13 @@
 package com.noveogroup.tas1;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.noveogroup.task1.R;
 
@@ -21,15 +18,11 @@ import java.util.Calendar;
  */
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    int DIALOG_DATE = 1;
-    int Year = 1990;
-    int Mounth = 1;
-    int Day = 1;
-
     EditText Name;
     EditText SecondName;
     TextView Date;
     Button btnNextActivity;
+    DialogFragment newFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,41 +34,39 @@ public class MainActivity extends Activity implements View.OnClickListener {
         SecondName = (EditText) findViewById(R.id.Famil);
 
         btnNextActivity = (Button) findViewById(R.id.buttonNextActivity);
-        btnNextActivity.setOnClickListener( this);
+        btnNextActivity.setOnClickListener(this);
     }
+
 
     public void onclick(View view) {
-        showDialog(DIALOG_DATE);
+        newFragment = new DatePickerFragment(view);
+        newFragment.show(getFragmentManager(), "datePicker");
     }
 
-    protected Dialog onCreateDialog(int id) {
-        if (id == DIALOG_DATE) {
-            DatePickerDialog tpd = new DatePickerDialog(this, myCallBack, Year, Mounth, Day);
-            return tpd;
-        }
-        return super.onCreateDialog(id);
-    }
+    private int counterAge() {
+        int Year = 2016;
+        int Mounth = 1;
+        int Day = 1;
 
-    DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            Year = year;
-            Mounth = monthOfYear + 1;
-            Day = dayOfMonth;
-            Date.setText(Day + "." + Mounth + "." + Year);
+        String str = Date.getText().toString();
+        String[] tmp = str.split("/");
+        if (tmp.length == 3) {
+            Day = Integer.parseInt(tmp[0]);
+            Mounth = Integer.parseInt(tmp[1]);
+            Year = Integer.parseInt(tmp[2]);
         }
-    };
-
-    private int CounterAge(){
         int age;
         Calendar c = Calendar.getInstance();
         int mount = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
         age = c.get(Calendar.YEAR);
+        if (Year >= age) {
+            return 0;
+        }
         age = age - Year;
         if (mount < Mounth-1) {
             age--;
-        } else if (mount == Mounth-1){
+        } else if (mount == Mounth-1) {
             if (day < Day) {
                 age--;
             }
@@ -88,7 +79,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Intent intent = new Intent(this, SecondActvity.class);
         intent.putExtra("name", Name.getText().toString());
         intent.putExtra("secondName", SecondName.getText().toString());
-        int age = CounterAge();
+        int age = counterAge();
         intent.putExtra("date", String.valueOf(age));
         startActivity(intent);
     }
