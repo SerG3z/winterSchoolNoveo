2205ffd2ba7f97a69b183e05d38df7ebf751c918
serg <serg3z.hard@gmail.com>
2016-03-17 23:14:50 +0600

    add simple toolbar with theme

--- university-android-task3_toolbar/app/src/main/java/com/noveogroup/task3/RootActivity.java
+++ university-android-task3_toolbar/app/src/main/java/com/noveogroup/task3/RootActivity.java
@@ -1,20 +1,42 @@
 package com.noveogroup.task3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class RootActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+
+        ButterKnife.bind(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.simple_toolbar_button)
+    public void onClick(){
+        Intent intent = new Intent(this, SimpleToolbarActivity.class);
+        startActivity(intent);
+    }
+
 
     public void dummy(View view) {
         Toast.makeText(this, R.string.dragon, Toast.LENGTH_SHORT).show();
     }
+
 }
---
+++ university-android-task3_toolbar/app/src/main/java/com/noveogroup/task3/SimpleToolbarActivity.java
@@ -0,0 +1,64 @@
+package com.noveogroup.task3;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by serg on 17.03.16.
+ */
+public class SimpleToolbarActivity extends AppCompatActivity {
+
+    @Bind(R.id.simple_toolbar)
+    Toolbar simpleToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_toolbar);
+        ButterKnife.bind(this);
+
+        simpleToolbar.setTitle(R.string.simple);
+        setSupportActionBar(simpleToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_custom_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.room:
+                Toast.makeText(getApplicationContext(), R.string.click_room, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.rowing:
+            Toast.makeText(getApplicationContext(), R.string.click_rowing, Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.setting:
+            Toast.makeText(getApplicationContext(), R.string.click_setting, Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+}
